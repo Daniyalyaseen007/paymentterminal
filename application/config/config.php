3e90a6d9b4aca0eb2c2b4721ab61d46c6b6e4422
Daniyal Yaseen <daniyal.yaseen@nextac.org>
2023-08-23 14:13:12 +0500

    changes for Config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/terminal2.0/dynamic2.0/';
+$config['base_url'] = 'https://terminal.zenedgesystems.co/';
 
 /*
 |--------------------------------------------------------------------------
